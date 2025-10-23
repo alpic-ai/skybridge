a5b495360c5b6cc17070ff9b658b0cb1e256cee0
Julien Vallini <vallinij@gmail.com>
2025-10-23 15:38:01 +0200

    Add vite plugin

--- src/web/index.ts
+++ src/web/index.ts
@@ -2,3 +2,4 @@ export { useOpenAiGlobal } from "./use-openai-global.js";
 export { useToolOutput } from "./use-tool-output.js";
 export * from "./types.js";
 export { mountWidget } from "./mount-widget.js";
+export { skybridge } from "./plugin.js";
