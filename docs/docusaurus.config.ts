177b39940193fd87cd5ff6013ed5036a229ab189
Charles Sonigo <charles.sonigo@gmail.com>
2025-11-24 16:00:53 +0100

    Clean up the starter docusaurus website

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Skybridge',
+  tagline: 'Skybridge is the TypeScript framework for building ChatGPT & MCP apps',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -75,9 +75,9 @@ const config: Config = {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'My Site',
+      title: 'Skybridge',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Skybridge Logo',
         src: 'img/logo.svg',
       },
       items: [
