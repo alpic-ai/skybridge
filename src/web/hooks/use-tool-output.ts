2bce608832f40ae92e39873dff7fb3a5b2b1d2fc
Julien Vallini <vallinij@gmail.com>
2025-11-28 16:33:53 +0000

    Update comments

--- src/web/hooks/use-tool-output.ts
+++ src/web/hooks/use-tool-output.ts
@@ -1,7 +1,7 @@
 import { useToolInfo } from "./use-tool-info.js";
 
 /**
- * @deprecated This hook is deprecated. Use `useTool()` instead and access the `output` property.
+ * @deprecated This hook is deprecated. Use `useToolInfo()` instead and access the `output` property.
  */
 export function useToolOutput() {
   const { output } = useToolInfo();
