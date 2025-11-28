7377f11fdd89fbcf05105cd42dea1b83207aca29
Frédéric Barthelet <frederic.barthelet@gmail.com>
2025-11-28 22:06:06 +0100

    Add status, isPending and isSuccess property on useToolInfo

--- src/web/hooks/use-tool-info.ts
+++ src/web/hooks/use-tool-info.ts
@@ -1,9 +1,64 @@
 import { useOpenAiGlobal } from "./use-openai-global.js";
+import { useEffect, useState } from "react";
+import type { UnknownObject } from "../types.js";
 
-export function useToolInfo() {
+type BaseToolState<
+  TStatus extends "pending" | "success",
+  ToolInput extends UnknownObject,
+  ToolOutput extends UnknownObject = UnknownObject,
+  ToolResponseMetadata extends UnknownObject = UnknownObject
+> = {
+  status: TStatus;
+  isPending: TStatus extends "pending" ? true : false;
+  isSuccess: TStatus extends "success" ? true : false;
+  input: ToolInput;
+  output: TStatus extends "success" ? ToolOutput : undefined;
+  responseMetadata: TStatus extends "success"
+    ? ToolResponseMetadata
+    : undefined;
+};
+
+type PendingToolState<ToolInput extends UnknownObject> = BaseToolState<
+  "pending",
+  ToolInput
+>;
+type SuccessToolState<
+  ToolInput extends UnknownObject,
+  ToolOutput extends UnknownObject,
+  ToolResponseMetadata extends UnknownObject
+> = BaseToolState<"success", ToolInput, ToolOutput, ToolResponseMetadata>;
+
+type ToolState<
+  ToolInput extends UnknownObject,
+  ToolOutput extends UnknownObject,
+  ToolResponseMetadata extends UnknownObject
+> =
+  | PendingToolState<ToolInput>
+  | SuccessToolState<ToolInput, ToolOutput, ToolResponseMetadata>;
+
+export function useToolInfo<
+  ToolInput extends UnknownObject = UnknownObject,
+  ToolOutput extends UnknownObject = UnknownObject,
+  ToolResponseMetadata extends UnknownObject = UnknownObject
+>() {
+  const [status, setStatus] = useState<"pending" | "success">("pending");
+  const output = useOpenAiGlobal("toolOutput") ?? undefined;
+  const responseMetadata = useOpenAiGlobal("toolResponseMetadata") ?? undefined;
+
+  useEffect(() => {
+    setStatus(
+      output === undefined && responseMetadata === undefined
+        ? "pending"
+        : "success"
+    );
+  }, [output, responseMetadata]);
+
   return {
-    output: useOpenAiGlobal("toolOutput"),
-    input: useOpenAiGlobal("toolInput"),
-    responseMetadata: useOpenAiGlobal("toolResponseMetadata"),
-  };
+    input: useOpenAiGlobal("toolInput")!,
+    status,
+    isPending: status === "pending",
+    isSuccess: status === "success",
+    output,
+    responseMetadata,
+  } as ToolState<ToolInput, ToolOutput, ToolResponseMetadata>;
 }
