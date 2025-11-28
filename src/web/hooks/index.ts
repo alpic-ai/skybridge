48b675ead3c4c23a36c8488217681dd8c9290bea
Julien Vallini <vallinij@gmail.com>
2025-11-28 17:49:34 +0000

    Fix build

--- src/web/hooks/index.ts
+++ src/web/hooks/index.ts
@@ -1,6 +1,5 @@
 export { useCallTool } from "./use-call-tool.js";
 export { useDisplayMode } from "./use-display-mode.js";
-export { useLayout } from "./use-layout.js";
 export { useLocale } from "./use-locale.js";
 export { useOpenExternal } from "./use-open-external.js";
 export { useOpenAiGlobal } from "./use-openai-global.js";
@@ -10,7 +9,5 @@ export { useTheme } from "./use-theme.js";
 export { useToolInfo } from "./use-tool-info.js";
 export { useToolOutput } from "./use-tool-output.js";
 export { useToolResponseMetadata } from "./use-tool-response-metadata.js";
-export { useTool } from "./use-tool.js";
 export { useUserAgent } from "./use-user-agent.js";
 export { useWidgetState } from "./use-widget-state.js";
-
