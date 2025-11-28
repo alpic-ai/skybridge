0dc2086b9a356986f1b7922f87066af36f81816b
Pedro Rodrigues <pedro.rodrigues@supabase.io>
2025-11-28 00:31:07 +0000

    add useSendFollowUpMessage hook

--- src/web/index.ts
+++ src/web/index.ts
@@ -2,6 +2,7 @@ export { useOpenAiGlobal } from "./use-openai-global.js";
 export { useToolOutput } from "./use-tool-output.js";
 export { useToolResponseMetadata } from "./use-tool-response-metadata.js";
 export { useCallTool } from "./use-call-tool.js";
+export { useSendFollowUpMessage } from "./use-send-follow-up-message.js";
 export * from "./types.js";
 export { mountWidget } from "./mount-widget.js";
 export { skybridge } from "./plugin.js";
