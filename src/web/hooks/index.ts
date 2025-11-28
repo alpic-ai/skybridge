f9c889725213c2ba869fe2e325b486cb89ad5806
Julien Vallini <vallinij@gmail.com>
2025-11-28 11:39:36 +0000

    Add useTool and useDisplayMode hooks

--- src/web/hooks/index.ts
+++ src/web/hooks/index.ts
@@ -1,8 +1,9 @@
+export { useCallTool } from "./use-call-tool.js";
+export { useDisplayMode } from "./use-display-mode.js";
 export { useOpenAiGlobal } from "./use-openai-global.js";
+export { useSendFollowUpMessage } from "./use-send-follow-up-message.js";
+export { useToolInfo } from "./use-tool-info.js";
 export { useToolOutput } from "./use-tool-output.js";
 export { useToolResponseMetadata } from "./use-tool-response-metadata.js";
-export { useCallTool } from "./use-call-tool.js";
+export { useTool } from "./use-tool.js";
 export { useWidgetState } from "./use-widget-state.js";
-export { useToolInfo } from "./use-tool-info.js";
-export { useDisplayMode } from "./use-display-mode.js";
-export { useSendFollowUpMessage } from "./use-send-follow-up-message.js";
