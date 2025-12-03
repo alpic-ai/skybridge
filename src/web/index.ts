f1722d862a4f23ff48303732929a81e0c5fbe713
Julien Vallini <vallinij@gmail.com>
2025-12-03 14:55:47 +0000

    Implement useWidgetStore

--- src/web/index.ts
+++ src/web/index.ts
@@ -2,5 +2,6 @@ export * from "./types.js";
 export * from "./data-llm.js";
 export { mountWidget } from "./mount-widget.js";
 export { skybridge } from "./plugin/plugin.js";
+export { createStore } from "./create-store.js";
 export * from "./hooks/index.js";
 export { generateHelpers } from "./generate-helpers.js";
