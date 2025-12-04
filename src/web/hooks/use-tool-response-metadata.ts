243866faa59408b3a9d9724bc80dd993ed936c45
Frédéric Barthelet <frederic.barthelet@gmail.com>
2025-12-04 16:50:15 +0100

    Remove deprecated hooks

--- src/web/hooks/use-tool-response-metadata.ts
+++
@@ -1,8 +0,0 @@
-import { useOpenAiGlobal } from "./use-openai-global.js";
-
-/**
- * @deprecated This hook is deprecated. Use `useToolInfo()` instead and access the `responseMetadata` property.
- */
-export function useToolResponseMetadata() {
-  return useOpenAiGlobal("toolResponseMetadata");
-}
