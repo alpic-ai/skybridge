1c620a2e58f3f12dae5ba23a34de0e333158a9a7
Julien Vallini <vallinij@gmail.com>
2025-12-18 18:22:45 +0000

    Add permission oicd

--- scripts/publish-test.sh
+++ scripts/publish-test.sh
@@ -7,12 +7,13 @@ TEST_VERSION="0.0.0-dev.${SHORT_SHA}.${SUFFIX}"
 
 echo "Creating test version: ${TEST_VERSION}"
 
-# Update versions first
-pnpm -C packages/core exec npm version "${TEST_VERSION}" --no-git-tag-version
-pnpm -C packages/devtools exec npm version "${TEST_VERSION}" --no-git-tag-version
+pnpm --filter skybridge exec npm version "${TEST_VERSION}" --no-git-tag-version
+pnpm --filter @skybridge/devtools exec npm version "${TEST_VERSION}" --no-git-tag-version
 
-# Now publish the packages
-pnpm --filter skybridge publish --tag dev --access public --no-git-checks
-pnpm --filter @skybridge/devtools publish --tag dev --access public --no-git-checks
+pnpm --filter skybridge exec npm publish --tag dev --access public --ignore-scripts
+
+unset NODE_AUTH_TOKEN NPM_TOKEN
+rm -f ~/.npmrc
+pnpm --filter @skybridge/devtools exec npm publish --tag dev --access public --ignore-scripts
 
 echo "Packages published successfully"
