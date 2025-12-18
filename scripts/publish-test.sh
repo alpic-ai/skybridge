621b78ecc8b486fbc3b1e6b6662f8ff57a9f0b04
Julien Vallini <vallinij@gmail.com>
2025-12-18 17:26:53 +0000

    Fix publishing

--- scripts/publish-test.sh
+++ scripts/publish-test.sh
@@ -8,8 +8,8 @@ TEST_VERSION="0.0.0-dev.${SHORT_SHA}.${SUFFIX}"
 echo "Creating test version: ${TEST_VERSION}"
 
 # Update versions first
-pnpm --filter skybridge version "${TEST_VERSION}" --no-git-tag-version
-pnpm --filter @skybridge/devtools version "${TEST_VERSION}" --no-git-tag-version
+pnpm -C packages/core exec npm version "${TEST_VERSION}" --no-git-tag-version
+pnpm -C packages/devtools exec npm version "${TEST_VERSION}" --no-git-tag-version
 
 # Now publish the packages
 pnpm --filter skybridge publish --tag dev --access public --no-git-checks
