828269f704ef429bcdd69b23411c9d4c73e5b699
Julien Vallini <vallinij@gmail.com>
2025-12-18 20:15:03 +0000

    Handle workspace

--- scripts/publish-test-packages.sh
+++ scripts/publish-test-packages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-pnpm --filter @skybridge/devtools exec npm publish --tag dev --access public --ignore-scripts
+pnpm --filter @skybridge/devtools publish --tag dev --access public --ignore-scripts --no-git-checks
 
 echo "Packages published successfully"
--- scripts/publish-test-skybridge.sh
+++ scripts/publish-test-skybridge.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-pnpm --filter skybridge exec npm publish --tag dev --access public --ignore-scripts
+pnpm --filter skybridge publish --tag dev --access public --ignore-scripts  --no-git-checks
 
 echo "Packages published successfully"
