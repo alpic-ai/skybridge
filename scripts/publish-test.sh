326a174022a5102f1b82da9b6b0ef94335cd5626
Julien Vallini <vallinij@gmail.com>
2025-12-18 15:03:56 +0000

    Fix test workflow

--- scripts/publish-test.sh
+++ scripts/publish-test.sh
@@ -6,20 +6,20 @@ TEST_VERSION="0.0.0-dev.${SHORT_SHA}"
 
 echo "Creating test version: ${TEST_VERSION}"
 
-# First do skybridge
+# Publish Skybridge
 cd packages/core
 npm version "${TEST_VERSION}" --no-git-tag-version
-cd ../..
 
 echo "Publishing skybridge to npm with 'dev' tag..."
-pnpm --filter skybridge publish --tag dev --access public --no-git-checks
+npm publish --tag dev --access public
 
-# Then devtools
-# cd packages/devtools
-# npm version "${TEST_VERSION}" --no-git-tag-version
-# cd ../..
+cd ../..
 
-# echo "Publishing @skybridge/devtools to npm with 'dev' tag..."
-# pnpm --filter @skybridge/devtools publish --tag dev --access public
+# Publish Devtools
+cd packages/devtools
+npm version "${TEST_VERSION}" --no-git-tag-version
 
-# echo "Packages published successfully"
+echo "Publishing @skybridge/devtools to npm with 'dev' tag..."
+npm publish --tag dev --access public
+
+echo "Packages published successfully"
