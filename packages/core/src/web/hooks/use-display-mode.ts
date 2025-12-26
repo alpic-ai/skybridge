6f2c8b7cb319b09c40ecda3cc1d3ed1ea02ac863
Quentin Churet <quentin@alpic.ai>
2025-12-26 13:59:46 +0100

    feat: get directly the bridge to not loose context

--- packages/core/src/web/hooks/use-display-mode.ts
+++ packages/core/src/web/hooks/use-display-mode.ts
@@ -1,14 +1,14 @@
 import { useCallback } from "react";
-import { getBridgeMethods } from "../bridges/get-bridge-methods.js";
+import { getBridge } from "../bridges/get-bridge.js";
 import { useBridge } from "../bridges/index.js";
 import type { DisplayMode } from "../types.js";
 
 export function useDisplayMode() {
   const displayMode = useBridge("displayMode");
-  const { requestDisplayMode } = getBridgeMethods();
+  const bridge = getBridge();
   const setDisplayMode = useCallback(
-    (mode: DisplayMode) => requestDisplayMode({ mode }),
-    [requestDisplayMode],
+    (mode: DisplayMode) => bridge.requestDisplayMode({ mode }),
+    [bridge],
   );
 
   return [displayMode, setDisplayMode] as const;
