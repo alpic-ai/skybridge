fd750d9ae0a76c1c543abb237b6bef6e71bb20cf
paulleseute <55874177+paulleseute@users.noreply.github.com>
2026-01-05 16:38:23 +0100

    fix: align template magic 8 ball text (#187)

--- packages/create-skybridge/template/server/src/server.ts
+++ packages/create-skybridge/template/server/src/server.ts
@@ -3,10 +3,6 @@ import { z } from "zod";
 
 const Answers = [
   "As I see it, yes",
-  "Ask again later",
-  "Better not tell you now",
-  "Cannot predict now",
-  "Concentrate and ask again",
   "Don't count on it",
   "It is certain",
   "It is decidedly so",
@@ -15,7 +11,6 @@ const Answers = [
   "My sources say no",
   "Outlook good",
   "Outlook not so good",
-  "Reply hazy, try again",
   "Signs point to yes",
   "Very doubtful",
   "Without a doubt",
