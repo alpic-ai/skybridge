b46356eaae94ba9e850a5a7c6338ae2701e0156d
Julien Vallini <vallinij@gmail.com>
2026-01-08 11:09:34 +0100

    Initialize CLI

--- packages/core/bin/dev.js
+++ packages/core/bin/dev.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env -S node --loader ts-node/esm --disable-warning=ExperimentalWarning
 
+<<<<<<< HEAD
 import { execute } from "@oclif/core";
 
 await execute({ development: true, dir: import.meta.url });
+=======
+import {execute} from '@oclif/core'
+
+await execute({development: true, dir: import.meta.url})
+>>>>>>> 4d48d5e (Initialize CLI)
--- packages/core/bin/run.js
+++ packages/core/bin/run.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
+<<<<<<< HEAD
 import { execute } from "@oclif/core";
 
 await execute({ dir: import.meta.url });
+=======
+import {execute} from '@oclif/core'
+
+await execute({dir: import.meta.url})
+>>>>>>> 4d48d5e (Initialize CLI)
