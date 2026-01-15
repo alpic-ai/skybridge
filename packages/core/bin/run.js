1ca65c4a10bcd615f9f088dff45d5a0c11eb8f9d
Julien Vallini <vallinij@gmail.com>
2026-01-15 14:59:21 +0100

    Implement use-forwarded-host flag

--- packages/core/bin/run.js
+++ packages/core/bin/run.js
@@ -2,4 +2,11 @@
 
 import { execute } from "@oclif/core";
 
+if (
+  process.argv.length === 2 ||
+  (process.argv.length >= 3 && process.argv[2]?.startsWith("-"))
+) {
+  process.argv.splice(2, 0, "dev");
+}
+
 await execute({ dir: import.meta.url });
