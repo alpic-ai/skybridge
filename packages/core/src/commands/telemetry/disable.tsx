cf54baa64454a34beebe10c09525ceb0c78232aa
Frédéric Barthelet <frederic.barthelet@gmail.com>
2026-01-15 17:04:26 +0100

    Add telemetry to posthog for skybridge cli command

---
+++ packages/core/src/commands/telemetry/disable.tsx
@@ -0,0 +1,35 @@
+import { Command } from "@oclif/core";
+import { Box, render, Text } from "ink";
+import { setEnabled } from "../../hooks/telemetry.js";
+
+export default class TelemetryDisable extends Command {
+  static override description = "Disable Skybridge telemetry";
+
+  public async run(): Promise<void> {
+    await this.parse(TelemetryDisable);
+    setEnabled(false);
+
+    const App = () => (
+      <Box flexDirection="column" padding={1}>
+        <Box>
+          <Text color="yellow">✓</Text>
+          <Text> Telemetry has been </Text>
+          <Text color="yellow" bold>
+            disabled
+          </Text>
+        </Box>
+        <Box marginTop={1}>
+          <Text color="gray">Config saved to ~/.skybridge/config.json</Text>
+        </Box>
+        <Box marginTop={1}>
+          <Text color="gray">
+            You can also set SKYBRIDGE_TELEMETRY_DISABLED=1 to disable telemetry
+            via environment variable.
+          </Text>
+        </Box>
+      </Box>
+    );
+
+    render(<App />);
+  }
+}
