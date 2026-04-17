5cb86df2f3d3fa8d54a33f6cc1c51f579cd350c3
Théo Léonard <theo@alpic.ai>
2026-04-17 13:55:19 +0200

    --wip-- [skip ci]

--- examples/everything/server/src/index.ts
+++ examples/everything/server/src/index.ts
@@ -1,3 +1,5 @@
+// import { userPromptMiddleware } from "@alpic-ai/insights";
+import { userPromptMiddleware } from "@alpic-ai/insights";
 import { McpServer } from "skybridge/server";
 import { z } from "zod";
 
@@ -7,41 +9,47 @@ const server = new McpServer(
     version: "0.0.1",
   },
   { capabilities: {} },
-).registerWidget(
-  "show-everything",
-  {
-    description: "A playground to discover the Skybridge framework",
-    _meta: {
-      ui: {
-        csp: {
-          redirectDomains: ["https://docs.skybridge.tech", "https://alpic.ai", "https://github.com"],
+)
+  .mcpMiddleware(userPromptMiddleware())
+  .registerWidget(
+    "show-everything",
+    {
+      description: "A playground to discover the Skybridge framework",
+      _meta: {
+        ui: {
+          csp: {
+            redirectDomains: [
+              "https://docs.skybridge.tech",
+              "https://alpic.ai",
+              "https://github.com",
+            ],
+          },
         },
       },
     },
-  },
-  {
-    description: "A simple greeting tool",
-    inputSchema: {
-      name: z.string().describe("The user name"),
-    },
-    _meta: {
-      "openai/widgetAccessible": true,
-    },
-  },
-  async ({ name }) => {
-    const structuredContent = {
-      greeting: `Hi ${name}, this tool response content is visible by both you and the LLM`,
-    };
-    return {
-      structuredContent,
-      content: [{ type: "text", text: JSON.stringify(structuredContent) }],
-      isError: false,
-      _meta: {
-        secret: "But _meta is only visible to you",
+    {
+      description: "A simple greeting tool",
+      inputSchema: {
+        name: z.string().describe("The user name"),
       },
-    };
-  },
-);
+      _meta: {
+        "openai/widgetAccessible": true,
+      },
+    },
+    async ({ name }) => {
+      const structuredContent = {
+        greeting: `Hi ${name}, this tool response content is visible by both you and the LLM`,
+      };
+      return {
+        structuredContent,
+        content: [{ type: "text", text: JSON.stringify(structuredContent) }],
+        isError: false,
+        _meta: {
+          secret: "But _meta is only visible to you",
+        },
+      };
+    },
+  );
 
 server.run();
 
