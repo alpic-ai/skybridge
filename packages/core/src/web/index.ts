33b137317cf6e92930bff32bcb591ab883744bc1
Julien Vallini <vallinij@gmail.com>
2026-04-29 11:01:16 +0200

    Reintroduce mountView

--- packages/core/src/web/index.ts
+++ packages/core/src/web/index.ts
@@ -3,6 +3,7 @@ export { createStore } from "./create-store.js";
 export * from "./data-llm.js";
 export { generateHelpers } from "./generate-helpers.js";
 export * from "./hooks/index.js";
+export { mountView } from "./mount-view.js";
 export type { SkybridgePluginOptions } from "./plugin/plugin.js";
 export { skybridge } from "./plugin/plugin.js";
 export * from "./types.js";
