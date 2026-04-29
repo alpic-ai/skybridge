efbdb09324a4fbc0266f880fff00f9f1f59b394c
Julien Vallini <vallinij@gmail.com>
2026-04-29 11:52:25 +0200

    feat: split vite plugin into skybridge/vite subpath export

--- packages/core/src/web/index.ts
+++ packages/core/src/web/index.ts
@@ -4,6 +4,4 @@ export * from "./data-llm.js";
 export { generateHelpers } from "./generate-helpers.js";
 export * from "./hooks/index.js";
 export { mountView } from "./mount-view.js";
-export type { SkybridgePluginOptions } from "./plugin/plugin.js";
-export { skybridge } from "./plugin/plugin.js";
 export * from "./types.js";
