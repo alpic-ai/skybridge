3fa6cdd8a10ccd4b3f7d9fa0745fd5eb6a6dd841
Julien Vallini <vallinij@gmail.com>
2026-05-25 15:20:25 +0200

    test(devtools): derive auth devtools URL from shared port constants

---
+++ packages/devtools/e2e/fixtures/ports.ts
@@ -0,0 +1,6 @@
+export const FIXTURE_PORT = 4101;
+export const FIXTURE_AUTH_PORT = 4102;
+export const DEVTOOLS_PORT = 5173;
+export const DEVTOOLS_AUTH_PORT = 5174;
+
+export const DEVTOOLS_AUTH_URL = `http://localhost:${DEVTOOLS_AUTH_PORT}`;
